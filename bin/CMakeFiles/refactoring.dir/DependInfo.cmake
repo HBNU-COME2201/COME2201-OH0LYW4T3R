260f34f5309905c42f13b52ef11ec571e1bfdf69
OH0LYW4T3R <tjdtnco@gmail.com>
2024-10-08 12:47:09 +0000

    homework 6week 2

---
+++ bin/CMakeFiles/refactoring.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/S20201919/COME2201-OH0LYW4T3R/src/agent.cpp" "CMakeFiles/refactoring.dir/src/agent.cpp.o" "gcc" "CMakeFiles/refactoring.dir/src/agent.cpp.o.d"
+  "/home/S20201919/COME2201-OH0LYW4T3R/src/app.cpp" "CMakeFiles/refactoring.dir/src/app.cpp.o" "gcc" "CMakeFiles/refactoring.dir/src/app.cpp.o.d"
+  "/home/S20201919/COME2201-OH0LYW4T3R/src/mmanager.cpp" "CMakeFiles/refactoring.dir/src/mmanager.cpp.o" "gcc" "CMakeFiles/refactoring.dir/src/mmanager.cpp.o.d"
+  "/home/S20201919/COME2201-OH0LYW4T3R/src/tinyxml2.cpp" "CMakeFiles/refactoring.dir/src/tinyxml2.cpp.o" "gcc" "CMakeFiles/refactoring.dir/src/tinyxml2.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
